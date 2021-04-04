e87112ee85ddbbe9baed82a0fff874837a76bda3
Joel <joel.edwardson1@gmail.com>
2021-04-04 22:25:51 +0100

    fixed bug where level not pulled from storage on starting chrome

--- background.js
+++ background.js
@@ -1,32 +1,36 @@
-let connectify = 5;
-
 chrome.runtime.onInstalled.addListener(() => {
-  console.log('bitconnectify starting...');
-  chrome.storage.sync.set({ connectify });
+	chrome.storage.sync.set({'connectify': 5}, function() {
+		console.log('bitconnectify starting at 5%...');
+	});
 });
 
+
+let connectify = 5;
+function readLevel() {
+	chrome.storage.sync.get(['connectify'], function(result) {
+		connectify = result['connectify'];
+		console.log(`read connectify as ${connectify}%`);
+	});
+}
+chrome.runtime.onStartup.addListener(readLevel);
 chrome.storage.onChanged.addListener(function(changes, namespace) {
-	for(var key in changes) {
-		if(key == 'connectify') {
-			var storageChange = changes[key];
-			console.log(`bitconnectify level now ${storageChange.newValue}%`);
-			connectify = storageChange.newValue;
-		}
-	}
-});
+	readLevel();
+})
+
 
 chrome.webRequest.onHeadersReceived.addListener(
-  function(details) {
-    if(details.type=="main_frame") {
-      console.log(`header received, bitconnect chance is ${connectify}%`);
-      if(Math.ceil(Math.random()*100) <= connectify) {
-	    console.log("bitconneeeeeeeccctt...");
-          return {
-	        "redirectUrl": "https://www.youtube.com/watch?v=Ne-J4pAKAT4",
-	      }
+	function(details) {
+		if(details.type=="main_frame") {
+			console.log(`header received, bitconnect chance is ${connectify}%`);
+			
+			if(Math.ceil(Math.random()*100) <= connectify) {
+				console.log("bitconneeeeeeeccctt...");
+				return {
+					"redirectUrl": "https://www.youtube.com/watch?v=Ne-J4pAKAT4",
+				}
+			}
 		}
-	  }
 	},
-  {urls: ["<all_urls>"]},
-  ["blocking"],
+	{urls: ["<all_urls>"]},
+	["blocking"],
 );
